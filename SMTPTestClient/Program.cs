1e6bdce934f7e6b67895444845fe197d16e35c82
Manuel Krebber <manuel.krebber@granikos.eu>
2015-04-10 10:22:38 +0200

    Added test SMTP client.

---
+++ SMTPTestClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace SMTPTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new SmtpClient("localhost", 1337))
+            {
+                var message = new MailMessage("tester@test.de", "tester2@test.de", "Subject", "Some body");
+
+                client.Credentials = new NetworkCredential("username", "password");
+
+                client.Send(message);
+            }
+        }
+    }
+}
