e7c649b985125b51566176819d6b8eed948735e7
Manuel Krebber <manuel.krebber@granikos.eu>
2016-05-21 13:36:34 +0200

    Experimented with IMAP libraries...

--- SMTPTestClient/Program.cs
+++ SMTPTestClient/Program.cs
@@ -1,38 +1,49 @@
 ﻿using System;
 using System.ComponentModel.Composition.Hosting;
+using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Security;
+using System.Reflection;
+using System.Text;
 using Granikos.NikosTwo.Core;
+// using Granikos.NikosTwo.ImapClient;
 using Granikos.NikosTwo.Service.ConfigurationService.Models;
 using Granikos.NikosTwo.Service.Models;
 using Granikos.NikosTwo.Service.Retention;
 using Granikos.NikosTwo.SmtpClient;
 using Granikos.NikosTwo.SmtpServer;
+using S22.Imap;
 
 // using System.Net.Mail;
 
 namespace SMTPTestClient
 {
+    class TL : TraceListener
+    {
+        readonly StringBuilder _sb = new StringBuilder();
+
+        public override void Write(string message)
+        {
+
+        }
+
+        public override void WriteLine(string message)
+        {
+            _sb.AppendLine(message.Replace("S -> ", "> ").Replace("C -> ", "< "));
+        }
+
+        public string GetLog()
+        {
+            return _sb.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var basedir =
-                @"C:\Users\Manuel\Documents\Visual Studio 2013\Projects\NikosTwo\Granikos.NikosTwo.Service\bin\Debug\Logs\SystemLogs\Service";
-
-            var manager = new RetentionWorker(basedir);
-
-            manager.Config.MinTime = TimeSpan.FromDays(5);
-            // manager.Config.MaxTime = TimeSpan.FromDays(20);
-            // manager.Config.MaxFiles = 10;
-
-            manager.Config.MaxSize = 1024*1024;
-
-            manager.Run();
-
-            Console.ReadLine();
-
-            /*
             ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, errors) =>
             {
                 return true;
@@ -46,36 +57,47 @@ namespace SMTPTestClient
             var sc = new SendConnector
             {
                 Username = "357045ceeeb05fa28",
-                Password = "07db3fc0f21e46"
+                Password = "07db3fc0f21e46",
+                TLSSettings = new TLSSettings
+                {
+                    Mode = TLSMode.FullTunnel
+                }
             };
-            var client = SMTPClient.Create(container, sc, "localhost", 25);
 
-            var from = new MailAddress("manuel.krebber@granikos.eu");
-            var to = new MailAddress("manuel.krebber@outlook.com");
-
-            string mail = "Test";
-
-            var mailMessage = new Mail(from, new[] { to }, mail);
+            /*
+            var client = ImapClient.Create(sc, "imap.gmail.com", 993);
+            client.Stream.OnError += e => { throw e; };
 
             if (client.Connect())
             {
-                client.SendMail("manuel.krebber@granikos.eu", new[] {"manuel.krebber@outlook.com"}, mail);
-                client.Close();
-            }
+                Console.WriteLine("Connected");
 
-            /*
-            using (var client = new SmtpClient("localhost", 1337))
-            {
-                client.TLSFullTunnel = true;
-
-                var message = new MailMessage("tester@test.de", "tester2@test.de", "Subject", "Some body");
-
-                client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential("username", "password");
-
-                client.Send(message);
+                client.Stream.Write("CAPABILITY").OnOkay(resp =>
+                {
+                    Console.WriteLine("Okay, {0}", resp.Response);
+                });
             }
              * */
+            var field = typeof(ImapClient).GetField("ts", BindingFlags.NonPublic | BindingFlags.Static);
+
+            var ts = (TraceSource)field.GetValue(null);
+            var tl = new TL();
+            ts.Listeners.Add(tl);
+            ts.Switch.Level = SourceLevels.All;
+
+            using (var client = new ImapClient("imap.gmail.com", 993, true))
+            {
+                foreach (var cap in client.Capabilities())
+                {
+                    Console.WriteLine(cap);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("IMAP Log:");
+            Console.WriteLine(tl.GetLog());
+
+            Console.ReadLine();
         }
     }
 }
