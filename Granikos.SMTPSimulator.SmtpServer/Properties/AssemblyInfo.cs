c5df314ee32179231dc6cec19655fcb8fe2ee4f1
Manuel Krebber <admin@wheerd.de>
2017-04-11 17:25:04 +0200

    Removed some unnecessary files.

--- Granikos.SMTPSimulator.SmtpServer/Properties/AssemblyInfo.cs
+++ Granikos.SMTPSimulator.SmtpServer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("NikosTwoCore")]
+[assembly: AssemblyTitle("SMTPSimulatorCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Granikos GmbH & Co. KG")]
